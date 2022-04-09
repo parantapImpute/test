6f9c5e1cac0ff96093a043c0605f01b0405bbbf0
Parantap <>
2022-04-09 18:53:44 +0900

    lol
    jjj

--- Max_Sum_Contiguous_Subarray.cpp
+++ Max_Sum_Contiguous_Subarray.cpp
@@ -7,7 +7,7 @@ int Solution::maxSubArray(const vector<int> &A) {
         if(max_ending_here>max_so_far)
             max_so_far=max_ending_here;
         if(max_ending_here<0)
-            max_ending_here=0;
+            max_ending_here=1;
     }
     return max_so_far;
 }
